1ba6654d6b7600cf78e83888d8e3a3975a373964
Suraj Roy <surajroy2103@gmail.com>
2021-07-06 12:03:42 +0530

    INstalled drf, created model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    """
+    create columns :
+    title : to store blog title
+    sub title: to store blog sub title
+    author : to author name
+    blog cotent ,short description, image
+    created at field and updated at field 
+    """
+    title = models.CharField(max_length=255)
+    sub_title = models.CharField(max_length=255,blank=True,null=True)
+    author =models.CharField(max_length=120)
+    content = models.TextField()
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True,null=True)
+    draft =  models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    
--- lbsp/settings.py
+++ lbsp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #custom app
     'blog',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
